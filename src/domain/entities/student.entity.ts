67c279cce0606827d776fe952e398009a86b2855
iifawzi <iifawzie@gmail.com>
2022-07-31 03:25:21 +0200

    creating student entity
    Signed-off-by: iifawzi <iifawzie@gmail.com>

---
+++ src/domain/entities/student.entity.ts
@@ -0,0 +1,8 @@
+class Student {
+    constructor(private firstName: string, private lastName: string, private age: number, private grade: number)  {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.grade = grade;
+    }
+}
